d2bbc15d493f3f4790eab73bfee0c8a002ebb132
MaksusLord <106978825+MaksusLord@users.noreply.github.com>
2023-06-02 19:12:29 +0300

    j2

---
+++ JavaScript/lab2/lab2.js
@@ -0,0 +1,82 @@
+"use strict";
+
+function z1() {
+  function pow(x, n) {
+    if (n === 0) {
+      return 1;
+    } else if (n < 0) {
+      return 1 / pow(x, -n);
+    } else {
+      return x * pow(x, n - 1);
+    }
+  }
+  
+  let x = prompt("Введите число:");
+  let n = prompt("Введите степень:");
+  
+  alert(pow(x, n));
+}
+
+function z2() {
+  function sumTo(n) {
+    if (n === 1) {
+      return 1;
+    } else {
+      return n + sumTo(n - 1);
+    }
+  }
+  
+  alert(sumTo(100));
+}
+
+function z3() {
+  function factorial(n) {
+    if (n === 0) {
+      return 1n;
+    } else {
+      return BigInt(n) * factorial(n - 1);
+    }
+  }
+  
+  alert(factorial(5));
+}
+
+function z4() {
+  function fib(n) {
+    if (n === 0 || n === 1) {
+      return BigInt(n);
+    } else {
+      return fib(n - 1) + fib(n - 2);
+    }
+  }
+  
+  alert(fib(100));
+}
+
+function z5() {
+  function compare(x) {
+    return function(y) {
+      if (y > x) {
+        return true;
+      } else if (y < x) {
+        return false;
+      } else {
+        return null;
+      }
+    };
+  }
+  
+  alert(compare(5)(4));
+  alert(compare(5)(5));
+  alert(compare(5)(6));
+}
+
+function z6() {
+  function sum(...args) {
+    return args.reduce((acc, cur) => acc + cur, 0);
+  }
+  
+  alert(sum());
+  alert(sum(1));
+  alert(sum(1, 2));
+}
