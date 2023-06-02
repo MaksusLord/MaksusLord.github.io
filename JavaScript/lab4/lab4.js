9324fb93400f1349657227dd8a104c4a2baa80c2
MaksusLord <106978825+MaksusLord@users.noreply.github.com>
2023-06-02 21:51:17 +0300

    j4

--- JavaScript/lab4/lab4.js
+++ JavaScript/lab4/lab4.js
@@ -1,30 +1,38 @@
 // 1-3
 "use strict";
+
 function isEmpty(obj) {
-  return obj.hasOwnProperty()
+    return obj.hasOwnProperty()
 }
-let book1 = new Object(); // синтаксис "конструктор объекта"
-book1 = {
-  title: "13 drygalei oyshena",
-  pubYear: 2009,
-  price: 2847,
-  show: function () {
-    console.log(this.title);
-    console.log(this.price);
-  }
-};
+let book1 = {
+    title: "Гордость и предубеждение",
+    pubYear: 1796,
+    price: 360,
+    show: function () {
+        console.log(this.title);
+        console.log(this.price);
+    }
+};  // синтаксис "литерал объекта"
+
 console.log(isEmpty(book1));
+
 let book2 = new Object();
-book2.title = '10 negritat';
-book2.pubYear = 1908;
-book2.price = 200;
+book2.title = 'Обрыв';
+book2.pubYear = 1888;
+book2.price = 1000;
+
 for (let i in book2) console.log(book2[i]);
 console.log(isEmpty(book2));
-book1.show();
-book2.show = function showBook() {
-  console.log(this.title);
-  console.log(this.price);
+
+function showBook() {
+     console.log(this.title);
+     console.log(this.price);
 }
+
+book1.show();
+
+book2.show=showBook;
+
 book2.show();
 
 //4. Методы addClass и removeClass для объекта obj:
