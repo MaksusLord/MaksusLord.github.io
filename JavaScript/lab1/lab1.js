69225a1a0a4dfb09b4feff7582dd35a557fb356c
MaksusLord <106978825+MaksusLord@users.noreply.github.com>
2023-06-02 18:29:14 +0300

    j1

--- JavaScript/lab1/lab1.js
+++ JavaScript/lab1/lab1.js
@@ -1,21 +1,20 @@
 "use strict";
 
 function vozrast_pol() {
-    let age,gender;
-let age = prompt("Введите ваш возраст:");
-let gender = prompt("Введите ваш пол (мужской или женский):");
+  let age = prompt("Введите ваш возраст:");
+  let gender = prompt("Введите ваш пол (мужчина или женщина):");
 
-if (age >= 0 && age <= 17) {
-  alert("Вам работать еще рано - учитесь");
-} else if (age >= 18 && age <= 59 && gender === "мужской" || age >= 18 && age <= 54 && gender === "женский") {
-  alert("Вам еще работать и работать");
-} else if (age >= 60 && age <= 64 && gender === "мужской" || age >= 55 && age <= 59 && gender === "женский") {
-  alert("Скоро пенсия!");
-} else if (age > 65 && gender === "мужской" || age > 60 && gender === "женский") {
-  alert("Вам пора на пенсию");
-} else {
-  alert("Да кто ты такой?");
-}
+  if (age >= 0 && age <= 17) {
+    alert("Вам работать ещё рано — учитесь");
+  } else if ((age >= 18 && age <= 59 && gender === "мужчина") || (age >= 18 && age <= 54 && gender === "женщина")) {
+    alert("Вам ещё работать и работать");
+  } else if ((age >= 60 && age <= 64 && gender === "мужчина") || (age >= 55 && age <= 59 && gender === "женщина")) {
+    alert("Скоро пенсия!");
+  } else if (age > 65 && gender === "мужчина" || age > 60 && gender === "женщина") {
+    alert("Вам пора на пенсию");
+  } else {
+    alert("Да кто ты такой?");
+  }
 }
 
 function crow() {
