458f785ba8dd8a004d37b6503d96e1b12da2be25
MaksusLord <106978825+MaksusLord@users.noreply.github.com>
2023-06-02 19:50:56 +0300

    j2

--- JavaScript/lab2/lab2.js
+++ JavaScript/lab2/lab2.js
@@ -10,10 +10,6 @@ function pow(x, n) {
   }
 }
 
-let x = prompt("Введите число:");
-let n = prompt("Введите степень:");
-
-alert(pow(x, n));
 
 function sumTo(n) {
   if (n === 1) {
@@ -23,7 +19,6 @@ function sumTo(n) {
   }
 }
 
-alert(sumTo(100));
 
 function factorial(n) {
   if (n === 0) {
@@ -33,7 +28,6 @@ function factorial(n) {
   }
 }
 
-alert(factorial(5));
 
 function fib(n) {
   if (n === 0 || n === 1) {
@@ -42,8 +36,6 @@ function fib(n) {
     return fib(n - 1) + fib(n - 2);
   }
 }
-
-alert(fib(100));
 
 function compare(x) {
   return function(y) {
@@ -57,15 +49,7 @@ function compare(x) {
   };
 }
 
-alert(compare(5)(4));
-alert(compare(5)(5));
-alert(compare(5)(6));
-
 function sum(...args) {
   return args.reduce((acc, cur) => acc + cur, 0);
 }
-
-alert(sum());
-alert(sum(1));
-alert(sum(1, 2));
 
