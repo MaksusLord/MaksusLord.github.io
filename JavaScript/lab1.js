5f31c91cc0b6c5c13e90dae3067e8e5ffa53c8d7
MaksusLord <106978825+MaksusLord@users.noreply.github.com>
2023-06-02 17:59:50 +0300

    j1

--- JavaScript/lab1.js
+++ JavaScript/lab1.js
@@ -17,8 +17,81 @@ if (age >= 0 && age <= 17) {
 }
 }
 
-function vozrast_pol() {
+function crow() {
+    let num = parseInt(prompt("Введите число:"));
 
+    if (num === 1) {
+      alert(`На ветке сидит ${num} ворона`);
+    } else if (num >= 2 && num <= 4) {
+      alert(`На ветке сидят ${num} вороны`);
+    } else {
+      alert(`На ветке сидит ${num} ворон`);
+    }    
+}
 
+function number() {
+    let num;
+
+    do {
+      num = prompt("Введите число больше 100:", "");
+    } while (num <= 100 && num !== null);
     
+    alert(`Вы ввели число ${num}`);
+}
+
+function simple() {
+    let n = prompt("Введите число:");
+
+    for (let i = 2; i <= n; i++) {
+      let isPrime = true;
+    
+      for (let j = 2; j < i; j++) {
+        if (i % j === 0) {
+          isPrime = false;
+          break;
+        }
+      }
+    
+      if (isPrime) {
+        console.log(i);
+      }
+    }
+}
+
+function cat_dog() {
+    let numRows = prompt("Введите количество строк:");
+
+    let row = "";
+    
+    for (let i = 0; i < numRows; i++) {
+      row += "dog\t";
+      row += "dog\t";
+      row += "dog\t";
+      row += "cat\t";
+      row += "cat\t";
+      row += "dog\t\n";
+    
+      row += "dog\t";
+      row += "dog\t";
+      row += "cat\t";
+      row += "cat\t";
+      row += "dog\t";
+      row += "dog\t\n";
+    
+      row += "dog\t";
+      row += "cat\t";
+      row += "cat\t";
+      row += "dog\t";
+      row += "dog\t";
+      row += "dog\t\n";
+    
+      row += "cat\t";
+      row += "cat\t";
+      row += "dog\t";
+      row += "dog\t";
+      row += "dog\t";
+      row += "dog\t\n";
+    }
+    
+    console.log(row);
 }
