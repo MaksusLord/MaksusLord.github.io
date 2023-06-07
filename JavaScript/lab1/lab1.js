80eafe7f710e397a9a36d42d4628b2ba0df7118e
MaksusLord <106978825+MaksusLord@users.noreply.github.com>
2023-06-07 09:43:12 +0300

    j1

--- JavaScript/lab1/lab1.js
+++ JavaScript/lab1/lab1.js
@@ -60,7 +60,7 @@ function number() {
 
   do {
     number = prompt("Введите число больше 100:", "");
-  } while (number <= 100 && number != null);
+  } while (number <= 100 && number != null && number);
 
   if (number !== null) {
     alert("Спасибо, вы ввели число больше 100: " + number);
