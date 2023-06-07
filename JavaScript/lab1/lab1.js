90774a7a96a25d1b5860a30a8f10a20817d55af3
MaksusLord <106978825+MaksusLord@users.noreply.github.com>
2023-06-07 10:00:46 +0300

    j1

--- JavaScript/lab1/lab1.js
+++ JavaScript/lab1/lab1.js
@@ -94,11 +94,11 @@ function cat_dog() {
   let num = prompt("Введите количество строк: ");
   let k = Math.floor(num / 5);
   let l = num - 5 * k;
-  let one = "dog dog dog cat cat dog";
-  let two = "dog dog cat cat dog dog";
-  let three = "dog cat cat dog dog dog";
-  let four = "cat cat dog dog dog dog";
-  let five = "cat dog dog dog dog cat";
+  const one = "dog dog dog cat cat dog";
+  const two = "dog dog cat cat dog dog";
+  const three = "dog cat cat dog dog dog";
+  const four = "cat cat dog dog dog dog";
+  const five = "cat dog dog dog dog cat";
   var list = [one, two, three, four, five]
   for (let i = 1; i <= k; i++) {
     for (let j = 0; j < 5; j++) console.log(list[j]);
