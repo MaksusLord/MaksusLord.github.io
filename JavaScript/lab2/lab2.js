d82f685594bff43ed24ae636c1e5d3b4336c4d8c
MaksusLord <106978825+MaksusLord@users.noreply.github.com>
2023-06-07 08:58:43 +0300

    j2

--- JavaScript/lab2/lab2.js
+++ JavaScript/lab2/lab2.js
@@ -1,13 +1,34 @@
+"use strict";
+
+/**
+ * Функция, возвращающая x в степени n (n - целое число)
+ * @param {number} x - запрашиваемое числ, котоое возводится в степень n
+ * @param {number} n - степень, в которую возводится x
+ * @returns {number} - значение x, вовзведенное в степень 
+ */
+
 function pow(x, n)
 {
     return x**n;
 }
+
+/**
+ * вычисляет сумму чисел от 1 до n включительно
+ * @param {number} n - число, до которого необходимо рассчитать сумму
+ * @returns {number} - сумму всех чисел от n до 1 
+ */
 
 function sumTo(n)
 {
     return (n*(n+1))/2;  
 
 }
+
+/**
+ * возвращает факториал числа n!
+ * @param {number} n - число, для которого вычисляется факториал 
+ * @returns {BigInt} - Факториал заданного числа в виде BigInt
+ */
 
 function factorial(n)
 {
@@ -20,6 +41,12 @@ function factorial(n)
     }
     return res;
 }
+
+/**
+ * возвращает n-е число Фибоначчи
+ * @param {number} n - индекс желаемого числа Фибоначчи
+ * @returns  {BigInt} - n-е число Фибоначчи в виде BigInt
+ */
 
 function fib(n){
     a = BigInt(1);
@@ -40,6 +67,12 @@ function fib(n){
     }
 }
 
+/**
+ * принимает целочисленное значение x и возвращает анонимную функцию
+ * @param {number} x - значение, с которым нужно сравнить
+ * @returns {function} -  функция, которая принимает входное значение и возвращает логическое значение
+ */
+
 function compare(x)
 {
 return function(y)
@@ -49,6 +82,12 @@ return function(y)
     else if (y==x) return null;
 }
 }
+
+/**
+ * возвращает сумму всех своих аргументов
+ * @param  {...number} args - значения дя суммирования
+ * @returns {number} -  общая сумма всех аргументов
+ */
 
 function sum()
 {
